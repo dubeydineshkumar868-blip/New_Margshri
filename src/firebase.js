22151dbc6ce0486b846fa947fbe24f7d150e31b6
dubeydineshkumar868-blip <dubeydineshkumar868@gmail.com>
2026-09-04 21:58:55 +0530

    Add Firebase Authentication and Google Provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCIhHdQLY4187bNJyP6tPYFZoG9eOwaMmc",
@@ -12,3 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
